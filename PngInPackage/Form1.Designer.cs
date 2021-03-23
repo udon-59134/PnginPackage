3b81ca59153e1c282604ec070e83221ab2cb7e80
萃香うどん <suicaudon@gmail.com>
2021-03-23 18:52:08 +0900

    V1.0:新規作成

---
+++ PngInPackage/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace PngInPackage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.PngRefButton = new System.Windows.Forms.Button();
+            this.FileNameLabel1 = new System.Windows.Forms.Label();
+            this.SelectPngFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SelectAllFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.FileNameLabel2 = new System.Windows.Forms.Label();
+            this.FileRefButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.outputFileNameText = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ExecuteButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.バージョン情報AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.バージョン情報ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1.埋め込み対象となる画像を選択します。";
+            // 
+            // PngRefButton
+            // 
+            this.PngRefButton.Location = new System.Drawing.Point(14, 39);
+            this.PngRefButton.Name = "PngRefButton";
+            this.PngRefButton.Size = new System.Drawing.Size(75, 23);
+            this.PngRefButton.TabIndex = 1;
+            this.PngRefButton.Text = "参照";
+            this.PngRefButton.UseVisualStyleBackColor = true;
+            this.PngRefButton.Click += new System.EventHandler(this.PngRefButton_Click);
+            // 
+            // FileNameLabel1
+            // 
+            this.FileNameLabel1.AutoSize = true;
+            this.FileNameLabel1.Location = new System.Drawing.Point(93, 44);
+            this.FileNameLabel1.Name = "FileNameLabel1";
+            this.FileNameLabel1.Size = new System.Drawing.Size(123, 12);
+            this.FileNameLabel1.TabIndex = 2;
+            this.FileNameLabel1.Text = "ファイルを選択してください";
+            // 
+            // SelectPngFileDialog
+            // 
+            this.SelectPngFileDialog.Filter = "画像ファイル(*.png,*.PNG)|*.png;*.PNG";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "2.埋め込む対象のデータを選択します。";
+            // 
+            // SelectAllFileDialog
+            // 
+            this.SelectAllFileDialog.Filter = "すべてのファイル(*.*)|*.*";
+            // 
+            // FileNameLabel2
+            // 
+            this.FileNameLabel2.AutoSize = true;
+            this.FileNameLabel2.Location = new System.Drawing.Point(95, 85);
+            this.FileNameLabel2.Name = "FileNameLabel2";
+            this.FileNameLabel2.Size = new System.Drawing.Size(123, 12);
+            this.FileNameLabel2.TabIndex = 5;
+            this.FileNameLabel2.Text = "ファイルを選択してください";
+            // 
+            // FileRefButton
+            // 
+            this.FileRefButton.Location = new System.Drawing.Point(14, 80);
+            this.FileRefButton.Name = "FileRefButton";
+            this.FileRefButton.Size = new System.Drawing.Size(75, 23);
+            this.FileRefButton.TabIndex = 4;
+            this.FileRefButton.Text = "参照";
+            this.FileRefButton.UseVisualStyleBackColor = true;
+            this.FileRefButton.Click += new System.EventHandler(this.FileRefButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "3.出力するファイル名を選択します。";
+            // 
+            // outputFileNameText
+            // 
+            this.outputFileNameText.Location = new System.Drawing.Point(14, 121);
+            this.outputFileNameText.Name = "outputFileNameText";
+            this.outputFileNameText.Size = new System.Drawing.Size(736, 19);
+            this.outputFileNameText.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 228);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(741, 210);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "4.実行ボタンを押します。";
+            // 
+            // ExecuteButton
+            // 
+            this.ExecuteButton.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ExecuteButton.Location = new System.Drawing.Point(12, 158);
+            this.ExecuteButton.Name = "ExecuteButton";
+            this.ExecuteButton.Size = new System.Drawing.Size(738, 64);
+            this.ExecuteButton.TabIndex = 10;
+            this.ExecuteButton.Text = "実行";
+            this.ExecuteButton.UseVisualStyleBackColor = true;
+            this.ExecuteButton.Click += new System.EventHandler(this.ExecuteButton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.バージョン情報AToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(765, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // バージョン情報AToolStripMenuItem
+            // 
+            this.バージョン情報AToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.バージョン情報ToolStripMenuItem});
+            this.バージョン情報AToolStripMenuItem.Name = "バージョン情報AToolStripMenuItem";
+            this.バージョン情報AToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.バージョン情報AToolStripMenuItem.Text = "ヘルプ(&H)";
+            // 
+            // バージョン情報ToolStripMenuItem
+            // 
+            this.バージョン情報ToolStripMenuItem.Name = "バージョン情報ToolStripMenuItem";
+            this.バージョン情報ToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.バージョン情報ToolStripMenuItem.Text = "バージョン情報(&A)";
+            this.バージョン情報ToolStripMenuItem.Click += new System.EventHandler(this.バージョン情報ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 450);
+            this.Controls.Add(this.ExecuteButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.outputFileNameText);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.FileNameLabel2);
+            this.Controls.Add(this.FileRefButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.FileNameLabel1);
+            this.Controls.Add(this.PngRefButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Png埋め込みツール";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button PngRefButton;
+        private System.Windows.Forms.Label FileNameLabel1;
+        private System.Windows.Forms.OpenFileDialog SelectPngFileDialog;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog SelectAllFileDialog;
+        private System.Windows.Forms.Label FileNameLabel2;
+        private System.Windows.Forms.Button FileRefButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox outputFileNameText;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button ExecuteButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem バージョン情報AToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem バージョン情報ToolStripMenuItem;
+    }
+}
+
---
+++ PngInPackage/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PngInPackage
+{
+    public partial class Form1 : Form
+    {
+        // ファイルサイズ上限(3MB)
+        private const int FILESIZE_LIMIT = 3000000;
+        // 選択する画像ファイルのフルパス(ファイル名含む)
+        private string inputFullFilePath = "";
+        // 埋め込みするファイルのフルパス(ファイル名含む)
+        private string embeddedFullFilePath = "";
+        // 出力する画像ファイルのフルパス(ファイル名含む)
+        private string outputFullFilePath = "";
+        // EXEファイルのディレクトリパス
+        private string exeFullFilePath = "";
+        private const string EXEFILE = "pack.exe";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 埋め込み対象となる画像ファイルの選択を行う
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PngRefButton_Click(object sender, EventArgs e)
+        {
+            if(SelectPngFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // 選択したファイル名をラベルに反映する
+                FileNameLabel1.Text = SelectPngFileDialog.FileName;
+                inputFullFilePath = SelectPngFileDialog.FileName;
+
+                // 選択したファイル名からファイル名のみを取得(拡張子無し)
+                string inputFileNameNoExt = Path.GetFileNameWithoutExtension(inputFullFilePath);
+                // 選択したファイル名から拡張子のみを取得
+                string inputFileNameExt = Path.GetExtension(inputFullFilePath);
+                // 出力するファイル名を生成する
+                string outputFileName = inputFileNameNoExt + "_ex" + inputFileNameExt;
+                // 編集用のテキストボックスに生成した出力するファイル名を表示
+                outputFileNameText.Text = outputFileName;
+            }
+        }
+        /// <summary>
+        /// 埋め込む対象のファイルの選択を行う
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileRefButton_Click(object sender, EventArgs e)
+        {
+            if (SelectAllFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo fileInfo = null;
+                // 選択したファイルからファイルサイズを取得する
+                try
+                {
+                    fileInfo = new FileInfo(SelectAllFileDialog.FileName);
+                    
+                }
+                catch (IOException ioex)
+                {
+                    MessageBox.Show(ioex.Message, "例外エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                //MessageBox.Show(fileInfo.Length.ToString());
+                // ファイルサイズが3MB以上ならエラーにする
+                if (fileInfo.Length > FILESIZE_LIMIT)
+                {
+                    MessageBox.Show("埋め込む対象のファイルに3MB以上のデータは指定できません", "ファイルサイズの上限エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                else
+                {
+                    // 選択したファイル名をラベルに反映する
+                    FileNameLabel2.Text = SelectAllFileDialog.FileName;
+                    // 埋め込むファイル名を格納する
+                    embeddedFullFilePath = SelectAllFileDialog.FileName;
+                }
+            }
+        }
+        /// <summary>
+        /// コマンドを組み立てて実行する
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExecuteButton_Click(object sender, EventArgs e)
+        {
+            bool error_flg = false;
+            List<string> error_msg_list = new List<string>();
+
+            // 選択する画像ファイル名が空の場合、エラーとする
+            if(inputFullFilePath == "")
+            {
+                error_msg_list.Add("・埋め込み対象となる画像のファイル名がありません。");
+                error_flg = true;
+            }
+
+            // 埋め込むファイル名が空の場合、エラーとする
+            if(embeddedFullFilePath == "")
+            {
+                error_msg_list.Add("・埋め込む対象となるデータのファイル名がありません。");
+                error_flg = true;
+
+            }
+
+            // 出力ファイル名が空の場合、エラーとする
+            if (outputFileNameText.Text == "")
+            {
+                error_msg_list.Add("・出力する対象となる画像のファイル名がありません。");
+                error_flg = true;
+            }
+
+            if (error_flg)
+            {
+                string error_msg = string.Join("\n", error_msg_list);
+                MessageBox.Show(error_msg, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // EXEファイルが格納されているファイルパスを組み立てる
+            exeFullFilePath = Path.GetDirectoryName(Application.ExecutablePath) + "\\" + EXEFILE;
+            // 出力するファイル名を組み立てる
+            outputFullFilePath = Path.GetDirectoryName(inputFullFilePath) + "\\" + outputFileNameText.Text;
+            // コマンドに必要な要素をリストに入れる
+            List<string> commandList = new List<string>();
+            commandList.Add(exeFullFilePath);
+            commandList.Add(inputFullFilePath);
+            commandList.Add(embeddedFullFilePath);
+            commandList.Add(outputFullFilePath);
+
+            // コマンドを組み立てる
+            string command = string.Join(" ", commandList);
+            //MessageBox.Show(command);
+            //textBox2.Text = command;
+
+            // 使用するexeファイルの存在チェック
+            if (!File.Exists(EXEFILE))
+            {
+                MessageBox.Show(EXEFILE + " が見つかりません。処理を実行できません。", "実行ファイルの不足エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // 実行ボタンを無効化
+            ExecuteButton.Enabled = false;
+
+            // コンソールを受け取るテキストボックスを初期化
+            textBox2.Clear();
+
+            Process p = new Process();
+            // コマンドプロンプトと同じように実行します
+            p.StartInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
+            p.StartInfo.Arguments = "/c " + command; // 実行するファイル名（コマンド）
+
+            p.StartInfo.CreateNoWindow = false;   // コンソール・ウィンドウは開かない
+            p.StartInfo.UseShellExecute = false; // シェル機能を使用しない
+            p.StartInfo.RedirectStandardOutput = true;   // <-- これが「標準出力」リダイレクト
+
+            try
+            {
+                p.Start();  // コマンドを実行します
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // 実行ボタンを有効化
+                ExecuteButton.Enabled = true;
+                return;
+            }
+            // 標準出力に出力しようとした内容を取得して
+            // 結果表示用テキストボックスに表示します
+            textBox2.Text = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            MessageBox.Show("処理が完了しました", "完了", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            // 実行ボタンを有効化
+            ExecuteButton.Enabled = true;
+        }
+
+        private void バージョン情報ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string ver = "1.0";
+            string author = "萃香うどん";
+            string vermsg = String.Format("バージョン: {0}\n作成者: {1}", ver, author);
+            MessageBox.Show(vermsg, "情報", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
